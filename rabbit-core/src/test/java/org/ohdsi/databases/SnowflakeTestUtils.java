f793f56fc5423a4755e8d3f4de1904a522251080
Jan Blom <jan.j.blom@gmail.com>
2023-10-04 17:55:46 +0200

    Clearer name for method

--- rabbit-core/src/test/java/org/ohdsi/databases/SnowflakeTestUtils.java
+++ rabbit-core/src/test/java/org/ohdsi/databases/SnowflakeTestUtils.java
@@ -7,14 +7,14 @@ public class SnowflakeTestUtils {
     public static void setSnowflakeTestConfig(DbSettings dbSettings) {
         // get Snowflake settings from the environment
         // this should become the OHDSI Snowflake test instance once that is available
-        dbSettings.server = checkAndGetEnv("SNOWFLAKE_TEST_SERVER");
-        dbSettings.user = checkAndGetEnv("SNOWFLAKE_TEST_USER");
-        dbSettings.password = checkAndGetEnv("SNOWFLAKE_TEST_PASSWORD");
-        dbSettings.database = checkAndGetEnv("SNOWFLAKE_TEST_DATABASE");
+        dbSettings.server = getenvOrFail("SNOWFLAKE_TEST_SERVER");
+        dbSettings.user = getenvOrFail("SNOWFLAKE_TEST_USER");
+        dbSettings.password = getenvOrFail("SNOWFLAKE_TEST_PASSWORD");
+        dbSettings.database = getenvOrFail("SNOWFLAKE_TEST_DATABASE");
         dbSettings.domain = dbSettings.database;
     }
 
-    private static String checkAndGetEnv(String name) {
+    private static String getenvOrFail(String name) {
         String value = System.getenv(name);
         if (StringUtils.isEmpty(value)) {
             throw new RuntimeException(String.format("Environment variable '%s' is not set.", name));
